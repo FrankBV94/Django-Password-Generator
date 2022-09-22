186005ce23db24007b79914c3fb00178111298c9
FrankBV94 <frankbotelle@gmail.com>
2022-09-22 13:22:19 -0400

    OK

--- django_password_generator/settings.py
+++ django_password_generator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-aicj8stpvr+b7tnuf(dl2o*&p_(-*e!c)6%wr*xd0ul58v+nga"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
